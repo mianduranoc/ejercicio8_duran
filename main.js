1b9cb1e8e25a497ca3541e99171aeaf932f06459
MIGUEL ANGEL DURAN OCAMPO <mianduranoc@ittepic.edu.mx>
2019-02-27 22:41:50 -0700

    Primer commit

---
+++ main.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+var schema=require('./schema');
+
+mongoose.connect('mongodb://localhost:27017/test');
+
+var User=mongoose.model('User2',schema,'users2');
+
+var user=new User({
+   name:'John Smith',
+   email:'john@smith.io'
+});
+User.create(user,error=>{
+
+});
+user.save(error=>{
+   if (error){
+       console.log(error);
+       process.exit(1);
+   }
+   console.log("Saved!!");
+   process.exit(0);
+});
